cc7520912c792dac55afd7b3ed2616eb94e10f5b
韩柳彤 <hanliutong@gmail.com>
2019-05-10 16:50:32 +0800

    定义接口、返回值 修复逻辑bug

---
+++ lex/define.h
@@ -0,0 +1,22 @@
+#define IDN 1
+#define INT8 8
+#define INT10 10
+#define IF 11
+#define THEN 12
+#define ELSE 13
+#define WHILE 14
+#define DO 15
+#define INT16 16
+#define ADD 17 // +
+#define SUB 18 // -
+#define MUL 19 // *
+#define DIV 20 // /
+#define LESS 21 // <
+#define MORE 22 // >
+#define LPAR 23 // (
+#define RPAR 24 // )
+#define EQU 25  // =
+#define SEMI 26 // ;
+#define SEMI 26 // ;
+#define LCUR 26 // {
+#define RCUR 26 // }
--- lex/test.cpp
+++ lex/test.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 #include <stdlib.h>
+#include "define.h"
 #include "lex.h"
 using namespace std;
-extern int AAA1;
+
+extern string IDN_NAME;
+extern int NUM;
+
 int main(){
-    char *test = "BEGIN 0 123";
-    char *test1 = "BEGIN 0 123";
-    cout << scan(test) << endl;
-    cout << scan(test) << endl;
-    cout << scan(test) << endl;
-    cout << scan(test) << endl;
-    cout << scan(test1) << endl;
-    cout << AAA1;
+    char *t = "if i iff else e el els elses then t th the then0 while w wh whi whil whilee do d dowhile";
+    while(*t){
+        if (scan(t) == IDN){
+            cout << "IND:" << IDN_NAME << endl;
+        }
+        }
+    cout << "finished\n";
+
+    t = "+ - * / > < = ( ) ;";
+    while(*t){
+            cout << scan(t) << endl;
+        }
+    cout << "finished\n";
+
+    t = "10 0 123 456 7890 8 00 070 013 16 0x0 0xa 0xf 0x12f";
+    while(*t){
+        cout << scan(t) << endl;
+    }
+    cout << "finished\n";
     return 0;
 }
